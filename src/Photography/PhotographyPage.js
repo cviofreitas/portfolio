afc497a5355f5f97e2075ce8d0b4ed56c4ea58dd
Caio Freiats <caio-freitas@hotmail.com>
2023-10-25 21:05:37 -0400

    added additional optimized images

--- src/Photography/PhotographyPage.js
+++ src/Photography/PhotographyPage.js
@@ -17,23 +17,22 @@ import LaurenHillside from './images2/LaurenHillside.jpg'
 import LightBreak from './images2/LightBreak.jpg'
 import Lot from './images2/Lot.jpg'
 import ManOnABoat from './images2/ManOnABoat.jpg'
-import NewYorkWorker from './images/NewYorkWorker.jpg'
-import NYGrind from './images/NYGrind.jpg'
-import OldTown from './images/OldTown.jpg'
-import Pond from './images/Pond.jpg'
-import Reflection from './images/Reflection.jpg'
-import Riding from './images/Riding.jpg'
-import Sign from './images/Sign.jpg'
-import StarryNight from './images/StarryNight.jpg'
-import TheRock from './images/TheRock.jpg'
-
-import ThroughTheTrees from './images/ThroughTheTrees.jpg'
-import TurquoiseSunset from './images/TurquoiseSunset.jpg'
-import Waiting from './images/Waiting.jpg'
-import Waterfall from './images/Waterfall.jpg'
-import WTC from './images/WTC.jpg'
-import Foggy from './images/Foggy.JPG'
-import StormRising from './images/StormRising.jpg'
+import NewYorkWorker from './images2/NewYorkWorker.jpg'
+import NYGrind from './images2/NYGrind.jpg'
+import OldTown from './images2/OldTown.jpg'
+import Pond from './images2/Pond.jpg'
+import Reflection from './images2/Reflection.jpg'
+import Riding from './images2/Riding.jpg'
+import Sign from './images2/Sign.jpg'
+import StarryNight from './images2/StarryNight.jpg'
+import TheRock from './images2/TheRock.jpg'
+import ThroughTheTrees from './images2/ThroughTheTrees.jpg'
+import TurquoiseSunset from './images2/TurquoiseSunset.jpg'
+import Waiting from './images2/Waiting.jpg'
+import Waterfall from './images2/Waterfall.jpg'
+import WTC from './images2/WTC.jpg'
+import Foggy from './images2/Foggy.JPG'
+import StormRising from './images2/StormRising.jpg'
 
 import './PhotographyPage.css'
 const PhotographyPage = () => {
@@ -50,52 +49,52 @@ const PhotographyPage = () => {
             <div className='container'>
 
 
-                <img loading='lazy' src={Classic} alt='Classic' />
-                <img loading='lazy' src={Ferry} alt='Ferry' />
-                <img loading='lazy' src={NYGrind} alt='New york Worker' />
+                <img src={Classic} alt='Classic' />
+                <img src={Ferry} alt='Ferry' />
+                <img src={NYGrind} alt='New york Worker' />
 
-                <img loading='lazy' src={WTC} alt='World Trade Center' />
-                <img loading='lazy' src={NewYorkWorker} alt='New York Worker' />
-                <img loading='lazy' src={GoldenGateBridge} alt='Golden Gate Bridge' />
+                <img src={WTC} alt='World Trade Center' />
+                <img src={NewYorkWorker} alt='New York Worker' />
+                <img src={GoldenGateBridge} alt='Golden Gate Bridge' />
 
-                <img loading='lazy' src={ThroughTheTrees} alt='Tree sunrise' />
-                <img loading='lazy' src={Lot} alt='Lot' />
-                <img loading='lazy' src={Coastal} alt='Coastal' />
+                <img src={ThroughTheTrees} alt='Tree sunrise' />
+                <img src={Lot} alt='Lot' />
+                <img src={Coastal} alt='Coastal' />
 
-                <img loading='lazy' src={LaurenHillside} alt='Lauren Hillside' />
-                <img loading='lazy' src={Isolated} alt='Isolated' />
-                <img loading='lazy' src={Heavenly} alt='Heavenly' />
+                <img src={LaurenHillside} alt='Lauren Hillside' />
+                <img src={Isolated} alt='Isolated' />
+                <img src={Heavenly} alt='Heavenly' />
 
-                <img loading='lazy' src={CreekBridge} alt='Creek Bridge' />
-                <img loading='lazy' src={HouseOnAPrarie} alt='House On A Prarie' />
-                <img loading='lazy' src={Horse} alt='Horse' />
+                <img src={CreekBridge} alt='Creek Bridge' />
+                <img src={HouseOnAPrarie} alt='House On A Prarie' />
+                <img src={Horse} alt='Horse' />
 
-                <img loading='lazy' src={OldTown} alt='Old Town' />
-                <img loading='lazy' src={LightBreak} alt='Light Break' />
-                <img loading='lazy' src={Pond} alt='Pond' />
+                <img src={OldTown} alt='Old Town' />
+                <img src={LightBreak} alt='Light Break' />
+                <img src={Pond} alt='Pond' />
 
-                <img loading='lazy' src={Sign} alt='Signs' />
-                <img loading='lazy' src={Reflection} alt='Reflection' />
-                <img loading='lazy' src={Riding} alt='Riding' />
+                <img src={Sign} alt='Signs' />
+                <img src={Reflection} alt='Reflection' />
+                <img src={Riding} alt='Riding' />
 
-                <img loading='lazy' src={TurquoiseSunset} alt='Coastal sunset' />
-                <img loading='lazy' src={TheRock} alt='Rock' />
-                <img loading='lazy' src={StarryNight} alt='Milky Way' />
+                <img src={TurquoiseSunset} alt='Coastal sunset' />
+                <img src={TheRock} alt='Rock' />
+                <img src={StarryNight} alt='Milky Way' />
 
-                <img loading='lazy' src={Waterfall} alt='Waterfall' />
-                <img loading='lazy' src={hawaii} alt='Golden Gate Bridge' />
-                <img loading='lazy' src={Waiting} alt='Waiting' />
+                <img src={Waterfall} alt='Waterfall' />
+                <img src={hawaii} alt='Golden Gate Bridge' />
+                <img src={Waiting} alt='Waiting' />
 
-                <img loading='lazy' src={LaurenCanonBeach} alt='Lauren Canon Beach' />
-                <img loading='lazy' src={Frozen} alt='Frozen' />
-                <img loading='lazy' src={Foggy} alt='Foggy path' />
+                <img src={LaurenCanonBeach} alt='Lauren Canon Beach' />
+                <img src={Frozen} alt='Frozen' />
+                <img src={Foggy} alt='Foggy path' />
 
 
-                <img loading='lazy' src={ManOnABoat} alt='Man on a boat' />
-                <img loading='lazy' src={StormRising} alt='Stormy beach' />
-                <img loading='lazy' src={HouseOfRefuge} alt='House Of Refuge' />
+                <img src={ManOnABoat} alt='Man on a boat' />
+                <img src={StormRising} alt='Stormy beach' />
+                <img src={HouseOfRefuge} alt='House Of Refuge' />
 
-                <iframe src="https://drive.google.com/file/d/1Ik81MnLoie_JGwY__EL6EwwVRdsgj3a4/preview" width="640" height="480" allow="autoplay"></iframe>
+
             </div>
 
 
