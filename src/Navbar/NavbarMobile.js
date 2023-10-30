3f393eaf9399d8ea28d7759a3bfe74d8ce5674a4
Caio Freiats <caio-freitas@hotmail.com>
2023-10-30 16:38:28 -0400

    updated mobile menu

--- src/Navbar/NavbarMobile.js
+++ src/Navbar/NavbarMobile.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useNavigate } from "react-router-dom";
+import { useClickAway } from '@uidotdev/usehooks';
 import './NavbarMobile.css'
 const NavbarMobile = () => {
     const [menuSelected, setMenuSelected] = useState(false)
@@ -9,13 +10,18 @@ const NavbarMobile = () => {
         navigate(option)
 
     }
+
+    const ref = useClickAway(() => {
+        setMenuSelected(false);
+    });
     return (
         <div
             className={`navbar-mobile ${menuSelected ? 'drop-down' : ''}`}>
             <button className='menu-button' onClick={() => setMenuSelected(!menuSelected)}>
                 +
             </button>
-            <div className={`navbar-option-container-mobile ${menuSelected ? 'show' : ''}`}>
+            <div className={`navbar-option-container-mobile ${menuSelected ? 'show' : ''}`}
+                ref={ref}>
                 <button
                     className="navbar-option-mobile"
                     onClick={() => {
